07061c2a25c8a58be9ca748cbede95ab64dfd754
kanouelm <ekanoune@yahoo.com>
2021-02-17 10:42:35 +0100

    Section 17 - Spring Cloud Config Server: creating the ConfigServer application with its dependency: Spring Cloud Config Server

---
+++ src/test/java/de/kanoune/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kanoune.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
