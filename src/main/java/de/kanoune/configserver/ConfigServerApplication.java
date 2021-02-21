280ac233e6e97afecd5562f04c32249bd523c159
kanouelm <ekanoune@yahoo.com>
2021-02-21 23:04:55 +0100

    Section-19 - Spring Cloud Config: File System Backend. Section-20 - Spring Cloud Config: Configuration for Multiple Microservices

--- src/main/java/de/kanoune/configserver/ConfigServerApplication.java
+++ src/main/java/de/kanoune/configserver/ConfigServerApplication.java
@@ -1,9 +1,17 @@
 package de.kanoune.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+//@Configuration
+//@EnableAutoConfiguration
 @SpringBootApplication
+@EnableConfigServer
+//@PropertySource("classpath:application.properties")
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
